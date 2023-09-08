c1eb56eec57d9a21e04496c11f204fdeaf4d4251
Godwin <godwinmail6@gmail.com>
2023-09-08 18:37:21 +0200

    added film images

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import TopBar from './components/TopBar';
 import Footer from './components/Footer';
 import SecondTopBar from './components/SecondTopBar';
 import GenreOne from './components/GenreOne';
+import GenreTwo from './components/GenreTwo';
+import GenreThree from './components/GenreThree';
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark">
       <TopBar />
       <SecondTopBar />
       <GenreOne />
-
+      <GenreTwo />
+      <GenreThree />
       <Footer />
     </div>
   );
